a575581a8d4f81a053ba660d79129e1b785e85b9
Anhlong-Vo <anhlong.vo.18@cnu.edu>
2020-09-19 00:19:28 -0400

    Score:70

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -14,6 +14,20 @@
 
 using namespace std;
 
+vector<process_stats> stats;
+
+bool compareProcessStats(const process_stats& b1,  const process_stats& b2) {
+	return b1.process_number < b2.process_number;
+}
+bool compareCPUStats(const process_stats& b1,  const process_stats& b2) {
+	return b1.cpu_time < b2.cpu_time;
+}
+bool compareStartStats(const process_stats& b1,  const process_stats& b2) {
+	return b1.start_time < b2.start_time;
+}
+bool compareIOStats(const process_stats& b1,  const process_stats& b2) {
+	return b1.io_time < b2.io_time;
+}
 //********************** private to this compilation unit **********************
 
 //if myString does not contain a string rep of number returns o
@@ -23,25 +37,79 @@ int stringToInt(const char *myString) {
 }
 
 int loadData(const char* filename, bool ignoreFirstRow) {
+	stats.clear();
+	ifstream inputfile;
+	inputfile.open(filename);
 
+	if (!inputfile.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
+
+	string line;
+	string token;
+	stringstream ss;
+
+
+	if (ignoreFirstRow == true) {
+		getline(inputfile, line);
+	}
+
+	while (!inputfile.eof()) {
+
+		getline(inputfile, line);
+		ss.str(line);
+		process_stats stat;
+
+		getline(ss, token, CHAR_TO_SEARCH_FOR);
+		stat.process_number = atoi(token.c_str());
+
+		getline(ss, token, CHAR_TO_SEARCH_FOR);
+		stat.start_time = atoi(token.c_str());
+
+		getline(ss, token, CHAR_TO_SEARCH_FOR);
+		stat.cpu_time = atoi(token.c_str());
+
+		getline(ss, token, CHAR_TO_SEARCH_FOR);
+		stat.io_time = atoi(token.c_str());
+
+		stats.push_back(stat);
+
+		ss.clear();
+
+	}
 	return SUCCESS;
 }
 
 
 //will sort according to user preference
 void sortData(SORT_ORDER mySortOrder) {
+	switch(mySortOrder) {
+	case PROCESS_NUMBER:
+		sort(stats.begin(), stats.end(), compareProcessStats);
+		break;
+	case CPU_TIME:
+		sort(stats.begin(), stats.end(), compareCPUStats);
+		break;
+	case IO_TIME:
+		sort(stats.begin(), stats.end(), compareIOStats);
+		break;
+	case START_TIME:
+		sort(stats.begin(), stats.end(), compareStartStats);
+		break;
+	}
 
 }
 
 process_stats getNext() {
 	process_stats myFirst;
-
+	stats.erase(stats.begin());
+	myFirst = stats.front();
 	return myFirst;
 }
 
 //returns number of process_stats structs in the vector holding them
 int getNumbRows(){
-	return 0;
+	return stats.size();
 }
 
 
